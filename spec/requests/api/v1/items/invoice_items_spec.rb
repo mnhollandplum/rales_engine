0b0311f668861944e8b379eabe22a36c54287571
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-12-02 13:15:25 -0700

    completes testing for item invoice items

--- spec/requests/api/v1/items/invoice_items_spec.rb
+++ spec/requests/api/v1/items/invoice_items_spec.rb
@@ -2,20 +2,38 @@ require 'rails_helper'
 
 describe 'Invoices Invoice Items API' do
   it "returns all invoice items for an Invoice " do
-    merchant = create(:merchant)
-   customer = create(:customer)
-   item = create(:item, merchant_id: merchant.id)
-   invoice = create(:invoice, merchant_id: merchant.id, customer_id: customer.id)
-   create_list(:invoice_item, 4, item_id: item.id, invoice_id: invoice.id)
+    merchant_1 = create(:merchant)
 
-   get "/api/v1/items/#{item.id}/invoice_items"
+    customer_1 = create(:customer)
 
-   expect(response).to be_successful
+    invoice_1 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id)
+    invoice_2 = create(:invoice, merchant_id: merchant_1.id, customer_id: customer_1.id)
 
-    # expect(response).to be_successful
-    # expect(invoice_items["data"].count).to eq(2)
-    # expect(invoice_items["data"].first["id"]).to eq(invoice_item_1.id.to_s)
-    # expect(invoice_items["data"].last["id"]).to eq(invoice_item_2.id.to_s)
-    # expect(invoice_items["data"]).not_to include(invoice_item_3.id.to_s)
+    item_1 = create(:item, merchant_id: merchant_1.id)
+    item_2 = create(:item, merchant_id: merchant_1.id)
+
+    invoice_item_1 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_1.id)
+    invoice_item_2 = create(:invoice_item, invoice_id: invoice_1.id, item_id: item_1.id)
+    invoice_item_3 = create(:invoice_item, invoice_id: invoice_2.id, item_id: item_2.id)
+
+    get "/api/v1/items/#{item_1.id}/invoice_items"
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_items["data"].count).to eq(2)
+    expect(invoice_items["data"].first["id"]).to eq(invoice_item_1.id.to_s)
+    expect(invoice_items["data"].last["id"]).to eq(invoice_item_2.id.to_s)
+    expect(invoice_items["data"]).not_to include(invoice_item_3.id.to_s)
+
+    get "/api/v1/items/#{item_2.id}/invoice_items"
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(invoice_items["data"].count).to eq(1)
+    expect(invoice_items["data"].first["id"]).to eq(invoice_item_3.id.to_s)
+    expect(invoice_items["data"]).not_to include(invoice_item_2.id.to_s)
+    expect(invoice_items["data"]).not_to include(invoice_item_1.id.to_s)
   end
 end

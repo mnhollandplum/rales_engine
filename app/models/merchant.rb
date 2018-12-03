18cad0d8a20b1348f1aff3ec1c403a1f86c82375
Nikki Holland-Plum <mnhollandplum@gmail.com>
2018-12-02 19:49:53 -0700

    completes most revenue merchants method and testing

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,13 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
+
+  def self.most_revenue(quantity)
+   select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+     .joins(:invoices, invoices: [:invoice_items, :transactions])
+     .where(transactions: {result: "success"})
+     .group(:id)
+     .order('revenue DESC')
+     .limit(quantity)
+   end
 end
